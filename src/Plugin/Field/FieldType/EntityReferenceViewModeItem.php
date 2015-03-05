9acabd9547de56981f32deafee7c5395619f1b88
Paul McKibben <paul.mckibben@mediacurrent.com>
2015-03-05 12:48:32 -0500

    Move field settings form to field item plugin

--- src/Plugin/Field/FieldType/EntityReferenceViewModeItem.php
+++ src/Plugin/Field/FieldType/EntityReferenceViewModeItem.php
@@ -11,6 +11,7 @@ use Drupal\Core\Field\FieldStorageDefinitionInterface;
 use Drupal\Core\TypedData\DataDefinition;
 use Drupal\Core\Field\EntityReferenceFieldItemList;
 use Drupal\entity_reference\ConfigurableEntityReferenceItem;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Plugin implementation of the 'Entity Reference View Mode' field type.
@@ -47,5 +48,64 @@ class EntityReferenceViewModeItem extends ConfigurableEntityReferenceItem {
     $properties['view_mode'] = DataDefinition::create('string')
       ->setLabel(t('View mode'));
     return $properties;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultFieldSettings() {
+    return array(
+      'view_mode_settings' => array (
+        'view_mode_selector_enabled' => FALSE,
+        'allowed_view_modes' => array(),
+      ),
+    ) + parent::defaultFieldSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fieldSettingsForm(array $form, FormStateInterface $form_state) {
+    $element = parent::fieldSettingsForm($form, $form_state);
+
+    $settings = $this->getSettings();
+    $selector_enabled = $settings['view_mode_settings']['view_mode_selector_enabled'];
+    $element['view_mode_settings'] = array(
+      '#type' => 'details',
+      '#title' => t('View Mode Settings'),
+      '#open' => true,
+    );
+    $element['view_mode_settings']['view_mode_selector_enabled'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('Enable view mode selector'),
+      '#description' => t('Allow selection of view mode per referenced entity'),
+      '#default_value' => $selector_enabled,
+    );
+
+    // Get entity type
+    $entity_type = $settings['target_type'];
+
+    // Get all available entity view modes.
+    $view_modes = \Drupal::entityManager()->getViewModes($entity_type);
+
+    // Get currently-selected view modes.
+    $current_view_modes = $settings['view_mode_settings']['allowed_view_modes'];
+
+    // Build choices: view modes for the target entity type.
+    $mode_choices = array();
+
+    foreach ($view_modes as $key => $view_mode) {
+      $id = str_replace('.', '__', $view_mode['id']);
+      $mode_choices[$id] = $view_mode['label'];
+    }
+
+    $element['view_mode_settings']['allowed_view_modes'] = array(
+      '#type' => 'checkboxes',
+      '#options' => $mode_choices,
+      '#title' => t('Select enabled view modes'),
+      '#default_value' => $current_view_modes,
+    );
+
+    return $element;
   }
 }
